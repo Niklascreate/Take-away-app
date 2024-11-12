65c1df6ef1c2b7814fdfbe708b044a193b948db1
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-11-12 14:04:47 +0100

    hämta meny

---
+++ takeaway-backend/takeaway/functions/Getdishes/index.js
@@ -0,0 +1,19 @@
+const { db } = require('../../services/index');
+const { sendResponse, sendError } = require('../../responses/index');
+
+exports.handler = async () => {
+    try {
+        
+        const result = await db.scan({
+            TableName: 'HerringDB'
+        });
+
+        
+        return sendResponse(200, result.Items);
+
+    } catch (error) {
+        return sendError(500, { message: error.message });
+    }
+}
+
+
