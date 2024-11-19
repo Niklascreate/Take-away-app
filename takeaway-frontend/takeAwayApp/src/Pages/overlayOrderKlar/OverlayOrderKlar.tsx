0bbcbc8c4283bb23037da6fa74ee4214657412b2
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-11-19 14:09:51 +0100

    OverlayOrderKlar

---
+++ takeaway-frontend/takeAwayApp/src/Pages/overlayOrderKlar/OverlayOrderKlar.tsx
@@ -0,0 +1,46 @@
+import './overlayOrderKlar.css';
+import { motion } from 'framer-motion';
+
+function OverlayOrderKlar() {
+  return (
+    <section className="overlayOrderKlar">
+      <h1 className="orderRubrik">Din order är klar om:</h1>
+      <p className="orderTid">15 minuter</p>
+
+      <div className="fishContainer">
+        <motion.div
+          className="fishWithText"
+          initial={{ x: '-150%' }}
+          animate={{ x: '150%' }}
+          transition={{
+            x: {
+              repeat: Infinity,
+              repeatType: 'loop',
+              duration: 3,
+              ease: 'linear',
+            }
+          }}
+        >
+          <motion.img
+            src="src/assets/dödFisk.svg"
+            alt="Surströmmingsfisk"
+            className="fishImage"
+            style={{ rotate: '45deg' }}
+          />
+          <motion.span className="rotatingText">Strömming på Språng</motion.span>
+        </motion.div>
+      </div>
+
+      <div className="footerText">
+        <img src="src/assets/Location.svg" alt="Location Icon" className="locationIcon" />
+        <div className="footerTextContent">
+          <p>Hämta din mat här:</p>
+          <p>Surströmmingsvägen 1</p>
+          <p>113 51 Norrland</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default OverlayOrderKlar;
