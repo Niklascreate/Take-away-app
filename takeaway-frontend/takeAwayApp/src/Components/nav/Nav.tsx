45199b6820f47c96ad08d07fd82479951ee78c80
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-11-20 11:23:54 +0100

    overlayLogin

--- takeaway-frontend/takeAwayApp/src/Components/nav/Nav.tsx
+++ takeaway-frontend/takeAwayApp/src/Components/nav/Nav.tsx
@@ -1,7 +1,11 @@
-import './nav.css'
+import { useState } from 'react';
+import './nav.css';
 import { Link } from 'react-router-dom';
+import OverlayInlog from '../overlayInlog/OverlayInlog'; // Importera OverlayInlog-komponenten
 
 export default function Nav() {
+  const [isOverlayVisible, setOverlayVisible] = useState(false);
+
   return (
     <section className="nav_container">
       <ul className="nav_list">
@@ -11,9 +15,17 @@ export default function Nav() {
         <Link to="/info">
           <img src="src/assets/info.svg" alt="Info" className="nav_icon" />
         </Link>
-        <li><img src="src/assets/cart.svg" alt="Cart" className="nav_icon" /></li>
-        <li><img src="src/assets/avatar.svg" alt="Profil" className="nav_icon" /></li>
+        <li>
+          <img src="src/assets/cart.svg" alt="Cart" className="nav_icon" />
+        </li>
+        {/* När användaren klickar på profilikonen ska overlay visas */}
+        <li onClick={() => setOverlayVisible(true)}>
+          <img src="src/assets/avatar.svg" alt="Profil" className="nav_icon" />
+        </li>
       </ul>
+
+      {/* Visa OverlayInlog när isOverlayVisible är true */}
+      {isOverlayVisible && <OverlayInlog onClose={() => setOverlayVisible(false)} />}
     </section>
-  )
+  );
 }
