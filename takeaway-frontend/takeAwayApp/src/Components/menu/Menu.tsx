8fa620ac9902814e47c4b6b7a9cb06c0ec0d7655
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-11-21 11:27:26 +0100

    meny fix

--- takeaway-frontend/takeAwayApp/src/Components/menu/Menu.tsx
+++ takeaway-frontend/takeAwayApp/src/Components/menu/Menu.tsx
@@ -1,16 +1,14 @@
-import './menu.css'
+import './menu.css';
 import { useEffect, useState } from 'react';
 import { fetchMenu } from '../../../api/api';
 import { Dish } from '../../../interface/interface';
 import OverlayMenyInfo from '../overlayMenyInfo/OverlayMenyInfo';
-
 
 const Menu = () => {
   const [menuItems, setMenuItems] = useState<Dish[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [selectedDish, setSelectedDish] = useState<Dish | null>(null);
-
 
   useEffect(() => {
     const getMenu = async () => {
@@ -19,7 +17,6 @@ const Menu = () => {
         setMenuItems(data);
         setLoading(false);
       } catch (err) {
-
         setError("Failed to load menu. Please try again later.");
         setLoading(false);
       }
@@ -39,11 +36,7 @@ const Menu = () => {
       <h1 className="menu_header">Meny</h1>
       {menuItems.map((dish) => (
         <section key={dish.id} className="menu" onClick={() => openOverlay(dish)}>
-
-      <h1 className="menu_header">meny</h1>
-      {menuItems.map((dish) => (
-        <section key={dish.id} className="menu">
-         <img src={dish.imageUrl} alt={dish.name} className="menu_img" />
+          <img src={dish.imageUrl} alt={dish.name} className="menu_img" />
           <section className="menu_descText">
             <h2 className="menu_dish">{dish.name}</h2>
             <p className="menu_desc">{dish.description}</p>
@@ -55,12 +48,10 @@ const Menu = () => {
       {selectedDish && (
         <OverlayMenyInfo closeOverlay={closeOverlay} dish={selectedDish} />
       )}
-
     </section>
   );
 };
 
 export default Menu;
-
 
 
